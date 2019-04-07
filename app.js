f29977997991b472a8c75eb9d82e9ea39387eb9a
Sarah Trapaga <trapagasarah@gmail.com>
2019-04-07 14:48:34 -0400

    draw board from arrays

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+const empty = ''
+const playerOne = '🦊'
+const playerTwo = '🐻'
+
+let gameBoard = [
+    [empty,empty,empty,empty,empty,empty,empty],
+    [empty,empty,empty,empty,empty,empty,empty],
+    [empty,empty,empty,empty,empty,empty,empty],
+    [empty,empty,empty,empty,empty,empty,empty],
+    [empty,playerTwo,empty,empty,playerOne,empty,empty],
+    [empty,playerOne,empty,playerTwo,empty,empty,empty],
+]
+
+
+
 $(function () {
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < gameBoard.length ; i++) {
         let row = $('<div></div>')
         row.addClass('row')
-        for (let j = 0; j < 7; j++) {
-            let gameSquare = $('<div></div>')
+        for (let j = 0; j < gameBoard[i].length; j++) {
+            let gameSquare = $(`<div>${gameBoard[i][j]}</div>`)
             gameSquare.addClass('game-square')
             row.append(gameSquare)
         }
         $('#game-board').append(row)
     }
+    
 })
