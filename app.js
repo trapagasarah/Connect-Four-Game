f9bfa0bd7ff2ec929a410c92b87571c4062149ba
Sarah Trapaga <trapagasarah@gmail.com>
2019-04-07 18:02:59 -0400

    place pieces and switch players

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
 const empty = ''
 const playerOne = '🦊'
 const playerTwo = '🐻'
+let currentPlayer = playerOne
 
 let gameBoard = [
     [empty, empty, empty, empty, empty, empty, empty],
     [empty, empty, empty, empty, empty, empty, empty],
     [empty, empty, empty, empty, empty, empty, empty],
     [empty, empty, empty, empty, empty, empty, empty],
-    [empty, playerTwo, empty, empty, playerOne, empty, empty],
-    [empty, playerOne, empty, playerTwo, empty, empty, empty],
+    [empty, empty, empty, empty, empty, empty, empty],
+    [empty, empty, empty, empty, empty, empty, empty],
 ]
 let drawBoard = () => {
+    $('#game-board').html('')
     for (let i = 0; i < gameBoard.length; i++) {
         let row = $('<div></div>')
         row.addClass('row')
         for (let j = 0; j < gameBoard[i].length; j++) {
             let gameSquare = $(`<div>${gameBoard[i][j]}</div>`)
             gameSquare.addClass('game-square')
+
+            gameSquare.click(function () {
+                insertPiece(j)
+            })
             row.append(gameSquare)
         }
         $('#game-board').append(row)
     }
 }
-
+let insertPiece = function (column) {
+    let didPlacePiece = false
+    for (let i = 5; i >= 0; i--) {
+        if (gameBoard[i][column] === empty && didPlacePiece === false) {
+            gameBoard[i][column] = currentPlayer
+            didPlacePiece = true
+        }
+    }
+    if (didPlacePiece){
+        changeTurn()
+    }
+    drawBoard()
+}
+let changeTurn = function() {
+    if (currentPlayer === playerOne){
+        currentPlayer = playerTwo
+    } else {
+        currentPlayer = playerOne
+    }
+}
 
 $(function () {
     drawBoard()
+
 
 })
